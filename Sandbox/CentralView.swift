cd1748bb8d2fecd4a557af66400ba819b6bc705e
rectangle0123 <rectangle0123@gmail.com>
2024-08-26 11:22:09 +0900

    リファクタリング

--- Sandbox/CentralView.swift
+++ Sandbox/CentralView.swift
@@ -33,7 +33,7 @@ struct CentralView: View {
                             .cornerRadius(4.0)
                     }
                     Button(action: {
-                        bluetoothManager.readCharacteristic()
+                        bluetoothManager.readCharacteristics()
                     }) {
                         Text("Read")
                             .frame(maxWidth: .infinity)
@@ -51,144 +51,154 @@ struct CentralView: View {
 
 // Bluetoothマネージャー
 class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
-    // サービスUUID
-    static let serviceUUID: CBUUID = {
-        guard let uuidString = Bundle.main.object(forInfoDictionaryKey: "BluetoothServiceUUID") as? String else {
-            fatalError("BluetoothServiceUUID is missing in Info.plist")
-        }
-        return CBUUID(string: uuidString)
-    }()
-    // サービスUUID（16ビット）
-    static let serviceUUID16bit: CBUUID = {
-        guard let uuidString = Bundle.main.object(forInfoDictionaryKey: "BluetoothServiceUUID16bit") as? String else {
-            fatalError("BluetoothServiceUUID16bit is missing in Info.plist")
-        }
-        return CBUUID(string: uuidString)
-    }()
-    // キャラクタリスティックUUID
-    static let characteristicUUID: CBUUID = {
-        guard let uuidString = Bundle.main.object(forInfoDictionaryKey: "BluetoothCharacteristicUUID") as? String else {
-            fatalError("BluetoothCharacteristicUUID is missing in Info.plist")
-        }
-        return CBUUID(string: uuidString)
-    }()
-
-    // セントラルマネージャー
+    // MARK: - Constants
+    static let serviceUUID = BluetoothManager.getUUID(forKey: "BluetoothServiceUUID", fatalMessage: "BluetoothServiceUUID is missing.")
+    static let serviceUUID16bit = BluetoothManager.getUUID(forKey: "BluetoothServiceUUID16bit", fatalMessage: "BluetoothServiceUUID16bit is missing.")
+    static let characteristicUUID = BluetoothManager.getUUID(forKey: "BluetoothCharacteristicUUID", fatalMessage: "BluetoothCharacteristicUUID is missing.")
+    
+    // MARK: - Properties
     var centralManager: CBCentralManager!
-    // 接続したペリフェラル
-    var targetPeripheral: CBPeripheral!
-    // ログ
+    var targetPeripheral: CBPeripheral?
+    var targetService: CBService?
     @Published var logs: [Log] = []
-
-    // 初期化
+    
+    // MARK: - Initialization
     override init() {
         super.init()
         centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-
-    // Bluetoothの状態が変わった場合のコールバック
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        guard central.state == .poweredOn else { return }
-    }
-
-    // ペリフェラルのスキャンを開始する
+    
+    // MARK: - EventAction
+    // ペリフェラルスキャン
     func startScanning() {
         log(text: "Start scaninng...")
-        // ペリフェラルのスキャンを開始する
         centralManager.scanForPeripherals(withServices: nil, options: nil)
     }
-
-    // ペリフェラルが見つかった場合のコールバック
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        log(text: "Found \(peripheral.name ?? "Unknown").")
-        // サービスUUIDが一致するペリフェラルのみ接続する
-        if let serviceUUIDs = advertisementData[CBAdvertisementDataServiceUUIDsKey] as? [CBUUID] {
-            if serviceUUIDs.contains(BluetoothManager.serviceUUID16bit) {
-                // 重複して接続しない
-                if targetPeripheral == nil || targetPeripheral.state != .connected {
-                    // ペリフェラルの参照を保持
-                    targetPeripheral = peripheral
-                    // ペリフェラルのデリゲートを設定する
-                    targetPeripheral.delegate = self
-                    // スキャンを停止する
-                    centralManager.stopScan()
-                    log(text: "Stopped Scanning.")
-                    // ペリフェラルに接続する
-                    centralManager.connect(peripheral, options: nil)
-                }
-            }
-        }
-    }
-
-    // ペリフェラルと接続した場合のコールバック
-    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        log(text: "Connected to \(peripheral.name ?? "Unknown").")
-        // ペリフェラルのサービスを検索する
-        peripheral.discoverServices([BluetoothManager.serviceUUID])
-    }
-
-    // ペリフェラルのサービスが見つかった場合のコールバック
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: (any Error)?) {
-        guard error == nil else {
-            log(text: "Error searching services: \(error!.localizedDescription)", error: true)
+    
+    // キャラクタリスティックス Read
+    func readCharacteristics() {
+        log(text: "Start Reading...")
+        guard let peripheral = targetPeripheral else {
+            log(text: "Error: Peripheral is not available.", error: true)
             return
         }
-        // サービスが存在するか確認する
-        guard let services = peripheral.services else { return }
-        for service in services {
-            log(text: "Found service: \(service.uuid)")
+        guard let service = targetService else {
+            log(text: "Error: Service is not available.", error: true)
+            return
+        }
+        // 接続したペリフェラルから取得したサービスのキャラクタリスティックスを検索する
+        peripheral.discoverCharacteristics(nil, for: service)
+        log(text: "Discovering characteristics.")
+    }
+    
+    // MARK: - CBCentralManagerDelegate
+    // Bluetooth状態検知コールバック
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        guard central.state == .poweredOn else {
+            log(text: "Error: Bluetooth is not available.", error: true)
+            return
         }
     }
-
-    // ペリフェラルが切断された場合のコールバック
+    
+    // ペリフェラル検知コールバック
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        log(text: "Received advertise: \(peripheral.name ?? "Unknown").")
+        // 指定するサービスUUIDを持つペリフェラルのみ接続する
+        // 重複して接続しない
+        guard let serviceUUIDs = advertisementData[CBAdvertisementDataServiceUUIDsKey] as? [CBUUID],
+              serviceUUIDs.contains(BluetoothManager.serviceUUID16bit),
+              targetPeripheral?.state != .connected else {
+            return
+        }
+        // ペリフェラルの参照を保持
+        targetPeripheral = peripheral
+        // ペリフェラルにデリゲートを設定する
+        targetPeripheral?.delegate = self
+        // スキャンを停止する
+        centralManager.stopScan()
+        log(text: "Stop Scanning.")
+        // ペリフェラルに接続する
+        centralManager.connect(peripheral, options: nil)
+        log(text: "Connecting: \(peripheral.name ?? "Unknown").")
+    }
+    
+    // ペリフェラル接続コールバック
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        log(text: "Connected: \(peripheral.name ?? "Unknown").")
+        // ペリフェラルのサービスを検索する
+        peripheral.discoverServices([BluetoothManager.serviceUUID])
+        log(text: "Discovering services.")
+    }
+    
+    // ペリフェラル切断コールバック
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-        log(text: "Disconnected from \(peripheral.name ?? "Unknown") \(peripheral.identifier)")
+        log(text: "Disconnected: \(peripheral.name ?? "Unknown") \(peripheral.identifier)")
         // ペリフェラルの参照を削除
         targetPeripheral = nil
     }
-
-    // キャラクタリスティックリードを実行
-    func readCharacteristic() {
-        log(text: "Start Reading...")
-        guard let services = targetPeripheral?.services else { return }
-        for service in services {
-            if service.uuid == BluetoothManager.serviceUUID {
-                targetPeripheral.discoverCharacteristics(nil, for: service)
-            }
-        }
-    }
-
-    // サービス内のキャラクタリスティックを検出した場合のコールバック
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: (any Error)?) {
+    
+    // MARK: - CBPeripheralDelegate
+    // サービス検知コールバック
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: (any Error)?) {
         guard error == nil else {
-            log(text: "Error searching characteristics: \(error!.localizedDescription)", error: true)
+            log(text: "Error discovering services: \(error!.localizedDescription)", error: true)
             return
         }
-        // キャラクタリスティックからデータを取得する
-        for characteristic in service.characteristics! {
-            log(text: "Found characteristics: \(characteristic.uuid)")
+        // サービスを取得する
+        guard let services = peripheral.services else {
+            log(text: "Error discovering services: Services not found.", error: true)
+            return
+        }
+        guard let service = services.first(where: { $0.uuid == BluetoothManager.serviceUUID }) else {
+            log(text: "Error discovering services: Target service not found.", error: true)
+            return
+        }
+        targetService = service
+        log(text: "Discovered service: \(service.uuid)")
+    }
+    
+    // キャラクタリスティックス検出コールバック
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: (any Error)?) {
+        guard error == nil else {
+            log(text: "Error discovering characteristics: \(error!.localizedDescription)", error: true)
+            return
+        }
+        log(text: "Discovering characteristics.")
+        // キャラクタリスティックスからデータを取得する
+        service.characteristics?.forEach { characteristic in
+            log(text: "Discovered characteristic: \(characteristic.uuid)")
             if characteristic.uuid == BluetoothManager.characteristicUUID {
                 peripheral.readValue(for: characteristic)
             }
         }
     }
-
-    // キャラクタリスティックの値が更新された場合のコールバック
+    
+    // Readレスポンス受信コールバック
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         guard error == nil else {
             log(text: "Error reading characteristic value: \(error!.localizedDescription)", error: true)
             return
         }
-        log(text: "Read characteristics: \(characteristic.uuid)")
         // データを読み取る
-        if let data = characteristic.value {
-            let text = String(data: data, encoding: .utf8) ?? "Invalid Data"
+        log(text: "Reading characteristics: \(characteristic.uuid)")
+        if let data = characteristic.value, let text = String(data: data, encoding: .utf8) {
             log(text: text, enhanced: true)
+        } else {
+            log(text: "Invalid Data", error: true)
         }
+    }
+    
+    // MARK: - Private Helper
+    // CBUUID生成
+    private static func getUUID(forKey key: String, fatalMessage: String) -> CBUUID {
+        guard let uuidString = Bundle.main.object(forInfoDictionaryKey: key) as? String else {
+            fatalError(fatalMessage)
+        }
+        return CBUUID(string: uuidString)
     }
 
     // ログ出力
-    func log(text: String, error: Bool = false, enhanced: Bool = false) {
+    private func log(text: String, error: Bool = false, enhanced: Bool = false) {
         DispatchQueue.main.async {
             self.logs.append(Log(text: text, error: error, enhanced: enhanced))
         }
